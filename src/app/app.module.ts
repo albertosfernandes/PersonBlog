b471f5deb6794c6da208b0217d657a7dab3b8f65
albertosfernandes <alberto.souzafernandes@gmail.com>
2021-12-10 22:53:42 -0300

    4 files modified, 6 added

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,10 +4,18 @@ import { FormsModule } from '@angular/forms';
 
 import { AppComponent } from './app.component';
 import { HelloComponent } from './hello.component';
+import { NavHeaderComponent } from './layout/header/navheader.component';
+import { SidebarComponent } from './layout/sidebar/sidebar.component';
+import { FooterComponent } from './layout/footer/footer.component';
 
 @NgModule({
   imports:      [ BrowserModule, FormsModule ],
-  declarations: [ AppComponent, HelloComponent ],
+  declarations: [ AppComponent, 
+                  HelloComponent,
+                  NavHeaderComponent,
+                  SidebarComponent,
+                  FooterComponent
+                ],
   bootstrap:    [ AppComponent ]
 })
 export class AppModule { }
---
+++ src/app/layout/header/navheader.component.ts
@@ -0,0 +1,8 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-navheader',
+  templateUrl: './navheader.component.html',
+  styles: [],
+})
+export class NavHeaderComponent {}
