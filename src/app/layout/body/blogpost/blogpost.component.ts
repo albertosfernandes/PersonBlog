9b037566afd092613b7ab2804758987c8af4538b
unknown <alberto.souzafernandes@gmail.com>
2022-05-25 23:17:27 -0300

    Primeiro post manual

--- src/app/layout/body/blogpost/blogpost.component.ts
+++ src/app/layout/body/blogpost/blogpost.component.ts
@@ -8,6 +8,10 @@ import { Component, OnInit } from '@angular/core';
 export class BlogpostComponent implements OnInit {
 
   constructor() { }
+  imageUrl1 = '../assets/post01/ASF-Infa01.drawio.png';
+  imageUrl2 = '../assets/post01/aws-route53-01.png';
+  imageUrl3 = '../assets/post01/aws-route53-02.png';
+  imageUrl4 = '../assets/post01/aws-route53-03.png';
 
   ngOnInit(): void {
   }
